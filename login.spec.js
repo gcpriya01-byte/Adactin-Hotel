de2bd8d0283f11ee1c56385052c345124fc2f046
gcpriya01-byte <gcpriya.01@gmail.com>
2026-09-16 11:58:31 +1000

    Update Playwright login test

--- login.spec.js
+++
@@ -1,8 +0,0 @@
-const {test, expect}= require('@playwright/test');
-test('Login', async({page})=>{
-    await page.goto('https://adactinhotelapp.com/');
-   await page.locator('#username').fill('priyagcp');
-   await page.locator('#password').fill('Adactin@123');
-   await page.getByRole('button',{type:'submit'}).click();
-   await page.waitForTimeout(5000);
-});
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,7 +1,7 @@
 const {test, expect}= require('@playwright/test');
 test('Login', async({page})=>{
     await page.goto('https://adactinhotelapp.com/');
-   await page.locator('#username').fill('priya');
+   await page.locator('#username').fill('priyagcp');
    await page.locator('#password').fill('Adactin@123');
    await page.locator('#login').click();
    await page.waitForTimeout(5000);
