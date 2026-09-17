a50802714b8248eba6540f0e89034c2376fde5e4
gcpriya01-byte <gcpriya.01@gmail.com>
2026-09-17 16:47:39 +1000

    Add test data

---
+++ utils/TestData.js
@@ -0,0 +1,45 @@
+exports.testData = {
+
+    // Login data
+    login: {
+        username: 'priyagcp',
+        password: 'Adactin@123'
+    },
+
+    // Search Hotel data
+    searchHotel: {
+        location: 'Melbourne',
+        hotel: 'Hotel Creek',
+        roomType: 'Double',
+        numberOfRooms: '3',
+        checkInDate: '03/11/2026',
+        checkOutDate: '13/11/2026',
+        adultsPerRoom: '3',
+        childrenPerRoom: '2'
+    },
+
+    // Select Hotel data
+    selectHotel: {
+        hotel: 'Hotel Creek',
+        location: 'Melbourne',
+        rooms: '2 Rooms',
+        arrivalDate: '03/11/2026',
+        departureDate: '13/11/2026',
+        numberOfDays: '10',
+        roomType: 'Double',
+        pricePerNight: 'AUD $ 225',
+        totalPriceExclGST: 'AUD $ 260'
+    },
+
+    // Book Hotel data
+    booking: {
+        firstName: 'Priya',
+        lastName: 'gcp',
+        billingAddress: 'Melbourne, Vic',
+        creditCardNumber: '1234567890123456',
+        creditCardType: 'VISA',
+        expiryMonth: 'May',
+        expiryYear: '2026',
+        cvvNumber: '678'
+    }
+};
